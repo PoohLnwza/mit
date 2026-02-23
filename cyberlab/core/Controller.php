d5edbd6996ddf94a82c0810a2bc406ec1b581324
Josu_Title <138316042+titledontcry@users.noreply.github.com>
2026-02-23 16:04:22 +0700

    Delete cyberlab directory

--- cyberlab/core/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Controller {
-  protected function render($viewPath) {
-    ob_start();
-    require __DIR__ . "/../app/views/$viewPath.php";
-    $content = ob_get_clean();
-    require __DIR__ . "/../app/views/layout.php";
-  }
-}

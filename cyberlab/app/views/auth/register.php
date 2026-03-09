d1f8445db82539c40316373e5f8f27144ad7e725
Josu_Title <138316042+titledontcry@users.noreply.github.com>
2026-03-09 16:38:36 +0700

    Delete cyberlab directory

--- cyberlab/app/views/auth/register.php
+++
@@ -1,5 +0,0 @@
-<form method="post" class="bg-white border-4 border-[#2d2a4a] p-4">
-  <input name="username" placeholder="username" class="border p-2 w-full mb-2">
-  <input type="password" name="password" placeholder="password" class="border p-2 w-full mb-2">
-  <button class="bg-[#ffd6e8] border-2 border-black w-full">▶ Submit</button>
-</form>
